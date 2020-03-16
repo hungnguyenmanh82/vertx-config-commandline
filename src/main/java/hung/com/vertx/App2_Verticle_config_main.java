cdd2d6cb1ba328b7dc48c66786a3eaaf156f7d97
DESKTOP-LBOHS1J\hungnm2 <hung.nguyen@netvisiontel.com>
2020-03-16 22:55:59 +0700

    update

--- src/main/java/hung/com/vertx/App2_Verticle_config_main.java
+++ src/main/java/hung/com/vertx/App2_Verticle_config_main.java
@@ -10,13 +10,14 @@ public class App2_Verticle_config_main extends AbstractVerticle {
 	public static void main(String[] args) {
 		Vertx vertx = Vertx.vertx();
 		
+		// Deploy ở source code sẽ ko lấy đc commandline config, vì ta ko dùng launcher, starter class.
 		vertx.deployVerticle(new App2_Verticle_config_main());
 	}
 
 	@Override
 	public void start(Future<Void> startFuture) throws Exception {	
 		super.start();
-		
+		// có thư viện riêng để lấy config từ file, env, system, http,...=> tiện hơn
 		System.out.println(config().toString());
 	}
 
