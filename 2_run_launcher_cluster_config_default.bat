495a4839974e2cc480b2fa92af14f5b040a1e88e
DESKTOP-LBOHS1J\hungnm2 <hung.nguyen@netvisiontel.com>
2021-01-25 15:00:29 +0700

    update

--- 2_run_launcher_cluster_config_default.bat
+++ 2_run_launcher_cluster_config_default.bat
@@ -2,6 +2,7 @@
 :: neu co exception va thieu lib thi cmd window of EasyShell se close ngay
 :: dung lenh "pause" window lai de xem exception
 :: user default cluster config with Hazelcast ( -cluster -ha )
+:: config defualt of Hazelcast = src/main/resources/hazelcast.xml
 java  -jar ./target/vertx-docker-config-launcher.jar -cluster -conf ./src/config/local.json
 
 :: lenh nay de CMD window ko bi close khi gap error
